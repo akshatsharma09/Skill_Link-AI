42029ace036c763f41eb35b0d3b2a67b9bda1d92
akshatsharma09 <akshats070205@gmail.com>
2025-12-28 18:33:03 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDco7WyT_LKqfVqckIUdlAX565LnYAl4PQ",
+  apiKey: "",
   authDomain: "skilllink-ai.firebaseapp.com",
   projectId: "skilllink-ai",
   storageBucket: "skilllink-ai.firebasestorage.app",
-  messagingSenderId: "298852762132",
-  appId: "1:298852762132:web:091d7287706af1ce465d01",
-  measurementId: "G-645R9CTP0C"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
